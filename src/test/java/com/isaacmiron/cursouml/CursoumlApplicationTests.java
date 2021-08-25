ff0b7ab3d3a66939fdc2a811ef39f81c8958bc13
Isaac Miron <isaac_miron@hotmail.com>
2021-08-25 15:54:59 -0300

    projeto criado

---
+++ src/test/java/com/isaacmiron/cursouml/CursoumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaacmiron.cursouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
